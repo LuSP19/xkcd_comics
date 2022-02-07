a29d2524fe5a6fe59d2a55cee77d52aeb77ecbf9
LuSP19 <48345486+LuSP19@users.noreply.github.com>
2022-02-07 23:20:18 +0500

    Сократил названия переменных

--- main.py
+++ main.py
@@ -25,17 +25,17 @@ def check_for_errors(decoded_response):
 
 def download_comic(num):
     url = f'https://xkcd.com/{num}/info.0.json'
-    xkcd_response = requests.get(url)
-    xkcd_response.raise_for_status()
-    xkcd_decoded_response = xkcd_response.json()
+    response = requests.get(url)
+    response.raise_for_status()
+    decoded_response = response.json()
 
-    comment = xkcd_decoded_response['alt']
+    comment = decoded_response['alt']
 
-    image_link = xkcd_decoded_response['img']
-    image_request_response = requests.get(image_link)
-    image_request_response.raise_for_status()
+    image_link = decoded_response['img']
+    response = requests.get(image_link)
+    response.raise_for_status()
     with open('comic.png', 'wb') as file:
-        file.write(image_request_response.content)
+        file.write(response.content)
 
     return comment
 
@@ -48,22 +48,22 @@ def upload_comic(group_id, vk_access_token, vk_api_version):
         'v': vk_api_version,
     }
 
-    upload_url_request_response = requests.get(url, params=params)
-    upload_url_request_response.raise_for_status()
-    decoded_upload_url_request_response = upload_url_request_response.json()
-    check_for_errors(decoded_upload_url_request_response)
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    decoded_response = response.json()
+    check_for_errors(decoded_response)
 
-    upload_url = decoded_upload_url_request_response['response']['upload_url']
+    upload_url = decoded_response['response']['upload_url']
     with open('comic.png', 'rb') as file:
         files = {'photo': file}
-        upload_request_response = requests.post(upload_url, files=files)
-    upload_request_response.raise_for_status()
-    decoded_upload_request_response = upload_request_response.json()
-    check_for_errors(decoded_upload_request_response)
+        response = requests.post(upload_url, files=files)
+    response.raise_for_status()
+    decoded_response = response.json()
+    check_for_errors(decoded_response)
 
-    photo = decoded_upload_request_response['photo']
-    server = decoded_upload_request_response['server']
-    hash = decoded_upload_request_response['hash']
+    photo = decoded_response['photo']
+    server = decoded_response['server']
+    hash = decoded_response['hash']
 
     return photo, server, hash
 
@@ -79,13 +79,13 @@ def save_comic(group_id, photo, server, hash, vk_access_token, vk_api_version):
         'v': vk_api_version,
     }
 
-    save_photo_request_response = requests.get(url, params=params)
-    save_photo_request_response.raise_for_status()
-    decoded_save_photo_request_response = save_photo_request_response.json()
-    check_for_errors(decoded_save_photo_request_response)
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    decoded_response = response.json()
+    check_for_errors(decoded_response)
 
-    media_id = decoded_save_photo_request_response['response'][0]['id']
-    owner_id = decoded_save_photo_request_response['response'][0]['owner_id']
+    media_id = decoded_response['response'][0]['id']
+    owner_id = decoded_response['response'][0]['owner_id']
 
     return owner_id, media_id
 
@@ -108,9 +108,9 @@ def publish_comic(
         'v': vk_api_version,
     }
 
-    publish_comic_request_response = requests.get(url, params=params)
-    publish_comic_request_response.raise_for_status()
-    check_for_errors(publish_comic_request_response.json())
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    check_for_errors(response.json())
 
 
 def main():
